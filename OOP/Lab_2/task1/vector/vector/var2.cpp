15a9c92269563a774d7e0eab14e3e06b8bd38124
Hemul997 <nikitamoshkin1997@gmail.com>
2016-10-30 12:25:55 +0300

    Был сделан рабочий вариант программы vector 2 var

---
+++ OOP/Lab_2/task1/vector/vector/var2.cpp
@@ -0,0 +1,14 @@
+// task1.cpp : Defines the entry point for the console application.
+
+#include "stdafx.h"
+#include "WorkWithVector.h"
+using namespace std;
+
+int main()
+{
+	vector<double> numbers = ReadVector(cin);
+	WorkWithVector(numbers);
+	SortVector(numbers);
+	OutVector(cout, numbers);
+	return 0;
+}
