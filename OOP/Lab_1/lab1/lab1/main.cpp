b17c2e9e89f1da9d48f9341f33ccfe96141561e6
Hemul997 <nikitamoshkin1997@gmail.com>
2017-01-13 09:52:04 +0300

    Изменены названия переменных

--- OOP/Lab_1/lab1/lab1/main.cpp
+++ OOP/Lab_1/lab1/lab1/main.cpp
@@ -15,33 +15,33 @@ int main(int argc, char * argv[])
 		}
 	}
 
-	ifstream fileOfInput;
-	fileOfInput.open(argv[1]);
-	if (!fileOfInput.is_open())
+	ifstream inputFile;
+	inputFile.open(argv[1]);
+	if (!inputFile.is_open())
 	{
 		cout << "Failed to open " << argv[1] << " for reading\n";
 		return 1;
 	}
 
-	ofstream output(argv[2]);
-	if (!output.is_open())
+	ofstream outputFile(argv[2]);
+	if (!outputFile.is_open())
 	{
 		cout << "Failed to open " << argv[2] << " for writing\n";
 		return 1;
 	}
 
 	Matrix matrix;
-	ReadMatrixFromFile(fileOfInput, matrix);
+	ReadMatrixFromFile(inputFile, matrix);
 	double determ = FindDeterminantOfMatrix(matrix);
 	if (determ == 0)
 	{
-		output << "Invert matrix doesn't exist, because determinant = 0\n";
+		outputFile << "Invert matrix doesn't exist, because determinant = 0\n";
 		return 1;
 	}
 	Matrix inverseMatrix;
 	InverseOfMatrix(matrix, inverseMatrix, determ);
-	PrintInvMatrix(inverseMatrix, output);
-	if (!output.flush())
+	PrintInvMatrix(inverseMatrix, outputFile);
+	if (!outputFile.flush())
 	{
 		cout << "Failed to save data on disk\n";
 		return 1;
