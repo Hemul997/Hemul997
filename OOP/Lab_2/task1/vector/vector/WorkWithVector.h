f15fdcafff020c31914cbe515a9e17359487856c
Hemul997 <nikitamoshkin1997@gmail.com>
2017-01-23 02:17:09 +0300

    Удалены ненужные файлы

--- OOP/Lab_2/task1/vector/vector/WorkWithVector.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <vector>
-using namespace std;
-
-vector<double> ReadVector(istream & input);
-void SortVector(vector<double> & numbers);
-void ProcessVector(vector<double> & numbers);
-void OutVector(ostream & output, vector<double> & numbers);
